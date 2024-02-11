e20634ed2fd41e4962fde7e10b404071f7870a7d
Dev <devendrapkmishra@gmail.com>
2024-02-11 12:52:35 +0530

    Hello React & Vite

--- HelloReact/README.md
+++ HelloReact/README.md
@@ -1,1 +1,7 @@
 # React + Vite
+
+-Started React
+-Covered Topics Like Class Component and Functional Component
+-File Structure (Was Different from the Learning Source due to use of Vite)
+-That's it for this folder.
+-11/2/24 
--- HelloReact/src/App.jsx
+++ HelloReact/src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App(props) {
   return (
     <>
-    <h1>
-      Hello World 
-    </h1>
+      <h1>
+        Hello {props.name}
+      </h1>
+      <h1>
+        Welcome to React + Vite
+      </h1>
+      <button>
+        {
+          props.loggedIn ? 'Welcome' : 'Please Login'
+        }
+      </button>
     </>
   )
 }
