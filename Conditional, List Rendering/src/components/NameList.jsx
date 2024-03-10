d9f7bb98ed411f6570eecfe4324fb3e6786353f7
Dev <devendrapkmishra@gmail.com>
2024-03-10 11:38:19 +0530

    Index as Key for Array Mapping

--- Conditional, List Rendering/src/components/NameList.jsx
+++ Conditional, List Rendering/src/components/NameList.jsx
@@ -23,15 +23,16 @@ function NameList() {
             skill: 'Vue'
         }
     ]
+    // Index as a Key generates a alot of problem.
+    // Use them only when the list is static, won't change, won't be filtered or resorted.
+    const namesList = names.map((name, index) => <h2 key={index}> {index} {name}</h2>)
 
-    const namesList = names.map(name => <h2>{name}</h2>)
-
-    const personList = persons.map(person => <Person key={person.id} person={person} />)
+    // const personList = persons.map(person => <Person key={person.id} person={person} />)
 
     return (
         <div>
-            {/* {namesList} */}
-            {personList}
+            {namesList}
+            {/* {personList} */}
         </div>
     )
 }
