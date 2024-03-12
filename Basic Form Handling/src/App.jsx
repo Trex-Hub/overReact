19853eac152d3196311bcdb4a0eaae35e6e73816
Dev <devendrapkmishra@gmail.com>
2024-03-12 10:42:54 +0530

    Basic Form handling with React

---
+++ Basic Form Handling/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Form from './components/Form'
+function App() {
+  return (
+    <div>
+      <h1>App Component</h1>
+      <Form />
+    </div>
+  )
+}
+
+export default App
