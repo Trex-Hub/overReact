e42fc6637048be2c6de091459bab3b2becffb9a4
Dev <devendrapkmishra@gmail.com>
2024-04-15 14:13:56 +0530

    Custom Hook - useDocumentTitle

---
+++ Custom Hooks/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DocTitleOne from './component/DocTitleOne'
+
+function App() {
+  return (
+    <div>
+      <DocTitleOne />
+    </div>
+  )
+}
+
+export default App
